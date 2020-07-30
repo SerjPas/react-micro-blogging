4fafbe2b0fa1fcc6a45dae709749d51915dfa67f
Serhii <Passerhii@gmail.com>
2020-07-30 15:03:06 +0300

    Milestone 5 - Deployment - done

---
+++ build/precache-manifest.e755e9e755fdec93e19a65a55bbe6664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19bc37fbb39d141e7eadb0a592e095fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c3eacf3868a71f79547",
+    "url": "/static/css/main.42d71014.chunk.css"
+  },
+  {
+    "revision": "8c349c381873d6566338",
+    "url": "/static/js/2.186abe82.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.186abe82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3eacf3868a71f79547",
+    "url": "/static/js/main.8bae1edb.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
