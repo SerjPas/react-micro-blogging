5c3d6c794da9f1d86e1e795d82a747ce8f3fb91b
Serhii <Passerhii@gmail.com>
2020-08-03 23:01:02 +0300

    firebase config added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyC6OyRCTsWh6ZvLUA8lQ2jgEvgw8ci5kno",
+    authDomain: "microblogging-project.firebaseapp.com",
+    databaseURL: "https://microblogging-project.firebaseio.com",
+    projectId: "microblogging-project",
+    storageBucket: "microblogging-project.appspot.com",
+    messagingSenderId: "45719062868",
+    appId: "1:45719062868:web:6ba70f40ada88e4342a0c1",
+    measurementId: "G-6KNN7QR1QK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics();
+
 
 ReactDOM.render(
   <React.StrictMode>
